e62dd1f18e42bfb314b62924c8a80c36e3807a5e
Jesús Enrique Rascón <blograso@gmail.com>
2022-03-28 18:04:17 +0200

    now we regenerate the bearer token for twitter

--- components/CalendarHeatMap.js
+++ components/CalendarHeatMap.js
@@ -1,70 +1,63 @@
 import EChartsReact from "echarts-for-react";
-import React from "react";
 import { countBy } from "lodash";
-const CalendarHeatMap = ({ data }) => {
-  const [option, setOption] = React.useState({});
 
+const CalendarHeatMap = ({ data }) => {
+  let refinedData = [];
   let dates;
   let groups;
-  let refinedData = [];
 
-  if (data) {
-    let tweets = data.data;
-    dates = tweets.map((t) => new Date(t.created_at));
+  console.log(data);
+  dates = data.map((t) => new Date(t.created_at));
 
-    groups = countBy(
-      dates,
-      (d) => new Date(d.getFullYear(), d.getMonth(), d.getDay())
-    );
+  groups = countBy(
+    dates,
+    (d) => new Date(d.getFullYear(), d.getMonth(), d.getDay())
+  );
 
-    Object.entries(groups).forEach(([k, v]) => {
-      refinedData.push([new Date(k), v]);
-    });
-  }
+  Object.entries(groups).forEach(([k, v]) => {
+    refinedData.push([new Date(k), v]);
+  });
 
-  React.useEffect(() => {
-    const bubbleChartOptions = {
-      title: {
-        top: 30,
-        left: "center",
-        text: "lets fucking go",
+  const bubbleChartOptions = {
+    title: {
+      top: 30,
+      left: "center",
+      text: "lets fucking go",
+    },
+    tooltip: {},
+    visualMap: {
+      min: 0,
+      max: 50,
+      type: "piecewise",
+      orient: "horizontal",
+      left: "center",
+      top: 65,
+    },
+    calendar: {
+      top: 120,
+      left: 30,
+      right: 30,
+      cellSize: ["auto", 13],
+
+      range: [new Date(2021, 11), new Date(2022, 12)],
+      itemStyle: {
+        borderWidth: 0.5,
       },
-      tooltip: {},
-      visualMap: {
-        min: 0,
-        max: 50,
-        type: "piecewise",
-        orient: "horizontal",
-        left: "center",
-        top: 65,
+      yearLabel: { show: true },
+    },
+    series: [
+      {
+        type: "heatmap",
+        coordinateSystem: "calendar",
+        calendarIndex: 0,
+        data: refinedData,
       },
-      calendar: {
-        top: 120,
-        left: 30,
-        right: 30,
-        cellSize: ["auto", 13],
+    ],
+  };
 
-        range: [new Date(2021, 11), new Date(2022, 12)],
-        itemStyle: {
-          borderWidth: 0.5,
-        },
-        yearLabel: { show: true },
-      },
-      series: [
-        {
-          type: "heatmap",
-          coordinateSystem: "calendar",
-          calendarIndex: 0,
-          data: refinedData,
-        },
-      ],
-    };
-
-    setOption(bubbleChartOptions);
-  }, []);
   return (
     <>
-      <EChartsReact option={option} />
+      <EChartsReact option={bubbleChartOptions} />
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
 import Head from "next/head";
 import CalendarHeatMap from "@/components/CalendarHeatMap";
-import styles from "../styles/Home.module.css";
 import YoutubeViewsCount from "@/components/YoutubeViewsCount";
 import Footer from "@/components/Footer";
-import useSWR from "swr";
-import fetcher from "@/lib/fetcher";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const { tweets } = useSWR("/api/tweets", fetcher);
-  const { youtubeData } = useSWR("/api/yt-video-plays", fetcher);
-  console.log(youtubeData);
+  const [tweets, setTweets] = useState();
+  //   const [youtubeData, setYoutubeData] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("/api/tweets");
+      const data = await response.json();
+
+      setTweets(data);
+    };
+
+    getData();
+  }, []);
+
+  //   useEffect(() => {
+  //     const getData = async () => {
+  //       const response = await fetch("/api/yt-video-plays");
+  //       const data = await response.json();
+
+  //       setYoutubeData(data);
+  //     };
+
+  //     getData();
+  //   }, []);
 
   return (
     <div className="container">
@@ -19,9 +38,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <YoutubeViewsCount data={youtubeData} />
+      {/* {youtubeData && <YoutubeViewsCount data={youtubeData} />} */}
 
-      {/* <CalendarHeatMap data={tweets} /> */}
+      {console.log("INDEX.JS", tweets)}
+      {tweets && <CalendarHeatMap data={tweets} />}
 
       <Footer />
     </div>
